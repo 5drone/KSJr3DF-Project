d583b462211e41871f94e705fb24c7f6fe3a7b0f
Young-Gi Yoon <seocomr@icloud.com>
2019-11-26 17:30:53 +0900

    VNC Viewer 변경에 따른 코드 변경

--- DockerVMAuto.py
+++ DockerVMAuto.py
@@ -10,51 +10,50 @@ import time
 # Docker Toolbox 경로
 dockertbx = path.expandvars(r'%ProgramFiles%\Docker Toolbox')
 
-# RealVNC Viewer 경로
-rVNCviewer = path.expandvars(r'%ProgramFiles%\RealVNC\VNC Viewer\vncviewer.exe')
+# TightVNC Viewer 경로
+tVNCviewer = path.expandvars(r'%ProgramFiles%\TightVNC\tvnviewer.exe')
 
 # 윈도우 이미지 자동화 도구 경로
 WinVMAuto = "%SETUPPATH%/WinVMAuto.exe"
-
-# docker container running on background
-dockerrun = 'docker run -itd'
-dockerath = 'docker attach'
 
 # 인자로 받기
 def receive_argu(string):
     # 인자 확인 후 다운로드 진행
     if (string == 1):
         print("Running Kali Linux on Docker ...")
-        subprocess.call([dockerrun,'-p','50000:5900','-p','50001:5901','5drone/kali:latest'])
-        # vnc 한번 연결했다가 끄고 다시 연결하는걸로
-        subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50001'])
+        subprocess.call(['docker','run','-itd','--name','kalivnc','-p','50001:5901','5drone/dockerhub:kali','/bin/bash'])
+        subprocess.call(['docker','exec','kalivnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50001','-password=5drone'])
     elif (string == 2):
         print("Running Ubuntu on Docker ...")
-        subprocess.call([dockerrun,'-p','50002:5900','-p','50003:5901','5drone/ubuntu:latest'])
-        subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50003'])
+        subprocess.call(['docker','run','-itd','--name','ubuntuvnc','-p','50002:5901','5drone/dockerhub:ubuntu','/bin/bash'])
+        subprocess.call(['docker','exec','ubuntuvnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50002','-password=5drone'])
     elif (string == 3):
         print("Running Securityonion on Docker ...")
-        subprocess.call([dockerrun,'-p','50004:5900','-p','50005:5901','5drone/onion:latest'])
-        subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50005'])
+        subprocess.call(['docker','run','-itd','--name','onionvnc','-p','50003:5901','5drone/dockerhub:onion','/bin/bash'])
+        subprocess.call(['docker','exec','onionvnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50003','-password=5drone'])
     elif (string == 4):
         print("Running VyOS on Docker ...")
-        subprocess.call([dockerrun,'-p','50006:5900','-p','50007:5901','5drone/vyos:latest'])
-        # subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50007'])
+        subprocess.call(['docker','run','-itd','--name','vyosvnc','-p','50004:5901','5drone/dockerhub:vyos','/bin/bash'])
+        subprocess.call(['docker','exec','vyosvnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50004','-password=5drone'])
     elif (string == 5):
         print("Running Metasploitable on Docker ...")
-        subprocess.call([dockerrun,'-p','50008:5900','-p','50009:5901','5drone/meta:latest'])
-        # subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50009'])
+        subprocess.call(['docker','run','-itd','--name','metavnc','-p','50005:5901','5drone/dockerhub:meta','/bin/bash'])
+        subprocess.call(['docker','exec','metavnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50005','-password=5drone'])
     elif (string == 6):
         print("Running CentOS on Docker ...")
-        subprocess.call([dockerrun,'-p','50010:5900','-p','50011:5901','5drone/centos:latest'])
-        subprocess.call([rVNCviewer,'192.168.99.100:1','-listen 50011'])
+        subprocess.call(['docker','run','-itd','--name','centosvnc','-p','50006:5901','5drone/dockerhub:centos','/bin/bash'])
+        subprocess.call(['docker','exec','centosvnc','vnc4server'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50006','-password=5drone'])
     elif (string == 7):
         print("Running bWASP on Docker ...")
-        subprocess.call(['docker','run','-itd','--name','beevnc','-p','5012:5900','-p','5013:5901','5drone/dockerhub:bee','/bin/bash'])
+        subprocess.call(['docker','run','-itd','--name','beevnc','-p','50007:5901','5drone/dockerhub:bee','/bin/bash'])
         subprocess.call(['docker','exec','beevnc','vnc4server'])
-        # subprocess.call(['vnc4server'])
-        # sleep(30)
-        subprocess.run([rVNCviewer,'192.168.99.100:1','-listen 5013'])
+        subprocess.run([tVNCviewer,'192.168.99.100::50007','-password=5drone'])
     elif (string == 8):
         print("Running Windows 7 on VMWare ...")
         subprocess.call([WinVMAuto,'1'])
@@ -75,11 +74,11 @@ if __name__ == '__main__':
         exit(0)
     print("Docker Detected")
 
-    print("Detecting RealVNC Viewer ...")
-    print(rVNCviewer)
-    if(os.path.isfile(rVNCviewer) == False):
-        print("RealVNC Viewer not Installed. Please Reinstall RealVNC Viewer.")
+    print("Detecting TightVNC Viewer ...")
+    print(tVNCviewer)
+    if(os.path.isfile(tVNCviewer) == False):
+        print("TightVNC Viewer not Installed. Please Reinstall TightVNC Viewer.")
         exit(0)
-    print("RealVNC Viewer Detected")
+    print("TightVNC Viewer Detected")
     
     receive_argu(int(sys.argv[1]))
